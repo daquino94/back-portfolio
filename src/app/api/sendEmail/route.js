76c0d791d88864b34f210762c382b4a80f59ce43
Daniel Aquino <daquino@pin-informatica.it>
2023-08-11 10:33:23 +0200

    cambiata response

--- src/app/api/sendEmail/route.js
+++ src/app/api/sendEmail/route.js
@@ -6,34 +6,28 @@ export const POST = async (req, res) => {
         const data = await req.json()
         console.log(process.env.NEXT_PUBLIC_MAIL_PORT)
         const transporter = nodemailer.createTransport({
-            host: process.env.NEXT_PUBLIC_MAIL_HOST, 
-            port: process.env.NEXT_PUBLIC_MAIL_PORT, 
+            host: process.env.NEXT_PUBLIC_MAIL_HOST,
+            port: process.env.NEXT_PUBLIC_MAIL_PORT,
             secure: true,
             auth: {
-              user: process.env.NEXT_PUBLIC_MAIL_USER, 
-              pass: process.env.NEXT_PUBLIC_MAIL_PASSWORD, 
+                user: process.env.NEXT_PUBLIC_MAIL_USER,
+                pass: process.env.NEXT_PUBLIC_MAIL_PASSWORD,
             },
-          });
+        });
 
-          const mailOptions = {
+        const mailOptions = {
             from: process.env.NEXT_PUBLIC_MAIL_MAIL_FROM,
-            to: process.env.NEXT_PUBLIC_MAIL_MAIL_TO, 
-            cc: process.env.NEXT_PUBLIC_MAIL_MAIL_CC, 
+            to: process.env.NEXT_PUBLIC_MAIL_MAIL_TO,
+            cc: process.env.NEXT_PUBLIC_MAIL_MAIL_CC,
             subject: 'Mail Dal sito',
             text: JSON.stringify(data)
-          };
-    
-          // Invia l'email
-          const info = await transporter.sendMail(mailOptions);
-    
-          console.log('Messaggio inviato:', info.messageId);
+        };
 
-        const response = await fetch(`https://jsonplaceholder.typicode.com/users`, {
-            method: "GET",
-            cache: 'no-store',
-        })
-        const responseData = await response.json()
-        return NextResponse.json(responseData)
+        const info = await transporter.sendMail(mailOptions);
+
+        console.log('Messaggio inviato:', info.messageId);
+
+        return NextResponse.json({ success: true, messageId: info.messageId })
     } catch (error) {
         console.log(error)
     }
