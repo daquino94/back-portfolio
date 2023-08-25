7d5e77624b86e5ce2892f01d973ba7fa731d8844
Daniel Aquino <daquino@pin-informatica.it>
2023-08-25 14:48:16 +0200

    testing all origin

--- next.config.js
+++ next.config.js
@@ -3,11 +3,10 @@ const nextConfig = {
     async headers() {
         return [
             {
-                // matching all API routes
                 source: "/api/:path*",
                 headers: [
                     { key: "Access-Control-Allow-Credentials", value: "true" },
-                    { key: "Access-Control-Allow-Origin", value: "aquinodaniel.com" }, // replace this your actual origin
+                    { key: "Access-Control-Allow-Origin", value: "*" },
                     { key: "Access-Control-Allow-Methods", value: "POST" },
                     { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
                 ]
