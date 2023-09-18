5622a4b07f1ef9fde9fa5d0b8e871db6e62a70e2
Daniel Aquino <daquino@pin-informatica.it>
2023-09-18 11:02:28 +0200

    cambiati header

--- next.config.js
+++ next.config.js
@@ -1,29 +1,29 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    async headers() {
-        return [
+  async headers() {
+    return [
+      {
+        // matching all API routes
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
           {
-            // matching all API routes
-            source: "/api/:path*",
-            headers: [
-              { key: "Access-Control-Allow-Credentials", value: "true" },
-              {
-                key: "Access-Control-Allow-Origin",
-                value: "*",
-              },
-              {
-                key: "Access-Control-Allow-Methods",
-                value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
-              },
-              {
-                key: "Access-Control-Allow-Headers",
-                value:
-                  "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Access-Control-Allow-Origin",
-              },
-            ],
+            key: "Access-Control-Allow-Origin",
+            value: "aquinodaniel.com",
           },
-        ];
-      }
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value:
+              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Access-Control-Allow-Origin",
+          },
+        ],
+      },
+    ];
+  }
 }
 
 module.exports = nextConfig
