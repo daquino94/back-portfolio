860072c8cdb4e0cf22ebb5b2fb3932e87c669b26
Daniel Aquino <daquino@pin-informatica.it>
2023-09-18 12:19:57 +0200

    bug

--- src/app/api/sendEmail/route.js
+++ src/app/api/sendEmail/route.js
@@ -6,7 +6,7 @@ export async function OPTIONS(request) {
   const response = new NextResponse(null, {
     status: 200,
     headers: {
-      "Access-Control-Allow-Origin": allowedOrigin || "https://aquinodaniel.com",
+      "Access-Control-Allow-Origin": allowedOrigin || "https://aquinodaniel.com/*",
       "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
       "Access-Control-Allow-Headers":
         "Content-Type, Authorization, X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Date, X-Api-Version",
